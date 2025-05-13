ec440c9f25c26bbbe855e0bfacd8503be5274683
Antonio.Barros.Alcantara.Neto <128563411+Antonio-BNeto@users.noreply.github.com>
2025-05-12 22:46:39 -0300

    Update requires app_send_mail

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -1,11 +1,11 @@
 <?php
 
-    require "./bibliotecas/PHPMailer/Exception.php";
+    require_once "./bibliotecas/PHPMailer/Exception.php";
     require_once "./bibliotecas/PHPMailer/OAuthTokenProvider.php";
-    require "./bibliotecas/PHPMailer/OAuth.php";
-    require "./bibliotecas/PHPMailer/PHPMailer.php";
-    require "./bibliotecas/PHPMailer/POP3.php";
-    require "./bibliotecas/PHPMailer/SMTP.php";
+    require_once "./bibliotecas/PHPMailer/OAuth.php";
+    require_once "./bibliotecas/PHPMailer/PHPMailer.php";
+    require_once "./bibliotecas/PHPMailer/POP3.php";
+    require_once "./bibliotecas/PHPMailer/SMTP.php";
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
